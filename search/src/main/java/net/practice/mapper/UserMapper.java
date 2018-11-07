64e2194cb08f45f0f5a922b268705fdc4b792bfd
KimJye <eoeogudgud@naver.com>
2018-11-07 11:30:28 +0900

    practice

---
+++ search/src/main/java/net/practice/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package net.practice.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import net.practice.dto.User;
+
+@Mapper
+public interface UserMapper {
+	List<User> findAllOrderBy(@Param("count") int count, @Param("order") String order);
+	List<User> findByNameOrUserid(User user);
+	List<User> findByNameOrUserid2(User user);
+	List<User> findByIdList(@Param("idList") int[] idList);
+	void update(User user);
+}
