94e1dfa186ec2a14b3d15716e4b94b91f776dddd
KimJye <eoeogudgud@naver.com>
2019-01-19 19:22:00 +0900

    spring form

---
+++ form0/src/main/java/model/Data.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Data {
+	String title;
+	int color;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getColor() {
+		return color;
+	}
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+}
