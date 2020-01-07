3e3a40ed5d4a637234db4b018eef7883cec8675a
KimJye <eoeogudgud@naver.com>
2020-01-07 17:12:12 +0900

    init

---
+++ yappian/src/main/java/com/yappian/yappian/YappianApplication.java
@@ -0,0 +1,13 @@
+package com.yappian.yappian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YappianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YappianApplication.class, args);
+    }
+
+}
---
+++ yappian/src/test/java/com/yappian/yappian/YappianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yappian.yappian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YappianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
