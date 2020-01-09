ef92922706512cdd1b74f2075aeb46d2b400ab38
KimJye <eoeogudgud@naver.com>
2020-01-10 03:36:12 +0900

    test : webEnvironment.RANDM_PORT

--- inflearn_spring_boot/springtestdemo/src/main/java/com/kimjye/springtestdemo/sample/SampleController.java
+++ inflearn_spring_boot/springtestdemo/src/main/java/com/kimjye/springtestdemo/sample/SampleController.java
@@ -10,8 +10,8 @@ public class SampleController {
     @Autowired
     private SampleService sampleService;
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
-        return "hello" + sampleService.getName();
+        return "hello " + sampleService.getName();
     }
 }
--- inflearn_spring_boot/springtestdemo/src/test/java/com/kimjye/springtestdemo/sample/SampleControllerTest.java
+++ inflearn_spring_boot/springtestdemo/src/test/java/com/kimjye/springtestdemo/sample/SampleControllerTest.java
@@ -5,28 +5,40 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
-
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class SampleControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
+     @Autowired
+     TestRestTemplate testRestTemplate;
 
-    @Test
-    public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello kimjye"))
-                .andDo(print());
-    }
+     @Test
+     public void hello() throws Exception {
+         String result = testRestTemplate.getForObject("/hello", String.class);
+         assertThat(result).isEqualTo("hello kimjye");
+     }
+
+     //    @Autowired
+    //    MockMvc mockMvc;
+    //
+    //    @Test
+    //    public void hello() throws Exception {
+    //        mockMvc.perform(get("/hello"))
+    //                .andExpect(status().isOk())
+    //                .andExpect(content().string("hello kimjye"))
+    //                .andDo(print());
+    //    }
 }
