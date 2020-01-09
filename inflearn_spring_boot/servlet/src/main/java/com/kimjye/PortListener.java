c789186b8b51e7a920536a7479edcb3996eaf1e7
KimJye <eoeogudgud@naver.com>
2020-01-09 14:33:30 +0900

    내장 서블릿 컨테이너의 이해

---
+++ inflearn_spring_boot/servlet/src/main/java/com/kimjye/PortListener.java
@@ -0,0 +1,16 @@
+package com.kimjye;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent>  {
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent servletWebServerInitializedEvent) {
+        ServletWebServerApplicationContext applicationContext = servletWebServerInitializedEvent.getApplicationContext();
+        applicationContext.getWebServer().getPort();
+    }
+}
