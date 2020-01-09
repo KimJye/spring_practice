5655a20940b56af4adafb1a1abc6a16f64582959
KimJye <eoeogudgud@naver.com>
2020-01-10 04:11:45 +0900

    test: @WebMvcTest를 사용한 슬라이싱 테스트

---
+++ inflearn_spring_boot/springtestdemo/src/test/java/com/kimjye/springtestdemo/sample/SliceTest.java
@@ -0,0 +1,33 @@
+package com.kimjye.springtestdemo.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SliceTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    SampleService sampleService;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello kimjye"))
+                .andDo(print());
+    }
+}
