7e47e454d3e5ef4f446b9db279967e0932e4a407
KimJye <eoeogudgud@naver.com>
2020-01-10 05:50:09 +0900

    spring web MVC - HttpMessageConverters
    Http요청 본문으로 들어오는 것을 객체로 변환하거나, 객체를 Http 응답
    본문으로 변경할 때 사용한다. 예를들어 @RequestBody, @ResponseBody를
    사용할 때 HttpMessageConverter가 사용된다.
    @Controller와 @ResponseBody를 함께 사용하지 않거나 @RestController로
    사용하지 않으면 MessageConverter로 타지않고 ViewNameResolver로 탄다.
    그렇게 되면 return 이름에 해당하는 View를 찾으려고 시도를 할 것이다.
    
    spring boot에서 View Resolver를 설정해주는 자동설정클래스가 있다.
    ContentNegotiatingViewResolver와 BeanNameViewResolver가 있다.
    ContentNegotiatingViewResolver는 들어오는 요청의 accept header에따라서
    응답이 달라지도록 도와준다. 즉 어떠한 요청이 들어오면 그 요청의 응답을
    만들 수 있는 모든 View를 찾아낸다. 그다음에 최종적으로 accept header와
    view 타입을 비교해서 선택을 한다.
    그런데 경우에 따라서 accept header를 제공하지 않는 요청들도 있다.
    이럴떄는 format이라는 파라미터를 사용한다.

--- inflearn_spring_boot/demospringbootmvc/src/main/java/com/kimjye/demospringbootmvc/user/UserController.java
+++ inflearn_spring_boot/demospringbootmvc/src/main/java/com/kimjye/demospringbootmvc/user/UserController.java
@@ -1,11 +1,15 @@
 package com.kimjye.demospringbootmvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
     @GetMapping("/hello")
     public String hello() {return "hello";}
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user){
+        return user;
+    }
 }
--- inflearn_spring_boot/demospringbootmvc/src/test/java/com/kimjye/demospringbootmvc/user/UserControllerTest.java
+++ inflearn_spring_boot/demospringbootmvc/src/test/java/com/kimjye/demospringbootmvc/user/UserControllerTest.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -24,5 +27,31 @@ public class UserControllerTest {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        String userJson="{\"username\":\"kimjye\", \"password\":\"123\"}";
+
+        // 요청을 만들고 응답을 확인하는 과정
+        // json으로 요청하고 xml으로 응답
+        mockMvc.perform(post("/users/create").
+                contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_XML)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/User/username").string("kimjye"))
+                .andExpect(xpath("User/password").string("123"));
+
+
+        // 요청을 만들고 응답을 확인하는 과정
+        // json으로 요청하고 json으로 응답
+//        mockMvc.perform(post("/users/create").
+//                contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON)
+//                .content(userJson))
+//            .andExpect(status().isOk())
+//            .andExpect(jsonPath("$.username", is(equalTo("kimjye"))))
+//            .andExpect(jsonPath("$.password", is(equalTo("123"))));
     }
 }
