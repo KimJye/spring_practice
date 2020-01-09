9ade79ed16a7be9e4acef683272a232563c24a42
KimJye <eoeogudgud@naver.com>
2020-01-10 03:48:10 +0900

    test: @MockBean

--- inflearn_spring_boot/springtestdemo/src/test/java/com/kimjye/springtestdemo/sample/SampleControllerTest.java
+++ inflearn_spring_boot/springtestdemo/src/test/java/com/kimjye/springtestdemo/sample/SampleControllerTest.java
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -25,10 +27,14 @@ public class SampleControllerTest {
      @Autowired
      TestRestTemplate testRestTemplate;
 
+     @MockBean
+     SampleService sampleService;
+
      @Test
      public void hello() throws Exception {
+         when(sampleService.getName()).thenReturn("ruby");
          String result = testRestTemplate.getForObject("/hello", String.class);
-         assertThat(result).isEqualTo("hello kimjye");
+         assertThat(result).isEqualTo("hello ruby");
      }
 
      //    @Autowired
