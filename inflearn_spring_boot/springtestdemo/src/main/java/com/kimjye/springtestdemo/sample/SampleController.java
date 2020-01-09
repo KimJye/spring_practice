a86b37587e93cdaec1a41756e575bfb9adbc109e
KimJye <eoeogudgud@naver.com>
2020-01-10 03:27:14 +0900

    test : webEnvironment.MOCK

---
+++ inflearn_spring_boot/springtestdemo/src/main/java/com/kimjye/springtestdemo/sample/SampleController.java
@@ -0,0 +1,17 @@
+package com.kimjye.springtestdemo.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @GetMapping
+    public String hello(){
+        return "hello" + sampleService.getName();
+    }
+}
---
+++ inflearn_spring_boot/springtestdemo/src/main/java/com/kimjye/springtestdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.kimjye.springtestdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "kimjye";
+    }
+}
