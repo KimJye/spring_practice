6e7b70668cd49df2e0eac042a5d658ba12ed7b5e
KimJye <eoeogudgud@naver.com>
2020-01-11 16:27:56 +0900

    정적 리소스

--- inflearn_spring_boot/demospringbootmvc/src/main/java/com/kimjye/demospringbootmvc/config/WebConfig.java
+++ inflearn_spring_boot/demospringbootmvc/src/main/java/com/kimjye/demospringbootmvc/config/WebConfig.java
@@ -1,8 +1,16 @@
 package com.kimjye.demospringbootmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /ruby/**로 요청이 들어오면 r디렉토리 밑에서 리소스를 제공하겠다.
+        registry.addResourceHandler("/ruby/**")
+                .addResourceLocations("classpath:/r/")
+                .setCachePeriod(20);
+    }
 }
